b0a31c0ca81de6e53224e731ff5e34d499dee509
devion-idle <devion.idle@gmail.com>
2014-04-04 01:20:52 -0400

    Add robot stats

--- web/giant_death_robot.dart
+++ web/giant_death_robot.dart
@@ -1,9 +1,16 @@
 import 'dart:html';
 import 'dart:async';
 
+Element robot_hull;
+Element robot_armor;
+Element robot_destruction;
+Element robot_carnage;
+
 void main() {
-  Duration updateTime = new Duration(seconds:5);
-  Timer timer = new Timer.periodic(updateTime, updatePhase);
+  globalQueries();
+  updateRobotStats();
+  Duration update_time = new Duration(seconds:5);
+  Timer timer = new Timer.periodic(update_time, updatePhase);
 }
 
 void updatePhase(Timer t) {
@@ -12,4 +19,23 @@ void updatePhase(Timer t) {
     phase.text = "Combat Phase";
   else
     phase.text = "Repair Phase";
+}
+
+void updateRobotStats() {
+  int hull = 3;
+  int armor = 5;
+  int destruction = 7;
+  int carnage = 9;
+  
+  robot_hull.text = "Hull: $hull";
+  robot_armor.text = "Armor: $armor";
+  robot_destruction.text = "Destruction: $destruction";
+  robot_carnage.text = "Carnage: $carnage";
+}
+
+void globalQueries() {
+  robot_hull = querySelector('#robot_hull');
+  robot_armor = querySelector('#robot_armor');
+  robot_destruction = querySelector('#robot_destruction');
+  robot_carnage = querySelector('#robot_carnage');
 }
